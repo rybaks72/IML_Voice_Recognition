92fdb3073dfa74367da9d3f2a8dd4b7f2f32bdfb
BanhBaoz <q.vuvan05@gmail.com>
2025-10-31 15:59:56 +0100

    fixed

--- preprocessing_pipeline/preprocessing_pipeline.py
+++ preprocessing_pipeline/preprocessing_pipeline.py
@@ -16,13 +16,12 @@ from pathlib import Path
 
 
 def create_directories():
-    for p in glob('./data/*/*/*'):
+    for p in glob('./data/*/*'):
         path,_ = os.path.splitext(p)
         path = "\\".join(["./spectogram_data", *path.split("\\")[1:]])
-        # print(path)
         directory = Path(path)
         directory.mkdir(parents=True, exist_ok=True)
-        print("Directory:", directory)
+        # print("Directory:", directory)
 
 
 sns.set_theme(style="white", palette = None)
@@ -51,12 +50,14 @@ def process_data(path, clip_length, label):
 
     path, _ = os.path.splitext(path)
     path = path.split("/")[2:][0].split("\\")
-    path.append(path[-1] + ".npz")
-    path = "/".join(["./spectogram_data", *path])
+    name = path[-1] + ".npz"
+    path.remove(path[-1])
+    path.append(name)
+    path = "/".join(["./spectogram_data", *path]).lower()
     X = np.array(spectograms)
     Y = np.array([label]*len(spectograms))
-    # print(f"X shape: {X.shape}")
-    # print(f"Y shape: {Y.shape}")
+    print(f"X shape: {X.shape}")
+    print(f"Y shape: {Y.shape}")
 
     np.savez_compressed(path, X=X, Y=Y)
     return spectograms
@@ -65,32 +66,15 @@ def process_data(path, clip_length, label):
 def convert_to_spectograms(clip_length):
     dowload_data()
 
-    # all_spectograms = []
-    # all_labels = []
-
     class0 = glob("./data/Class0/*/*.mp3")
     class1 = glob("./data/Class1/*/*.mp3")
 
-    #class0
     for clip in class0:
         specs = process_data(clip, clip_length, 0)
-        # all_spectograms.extend(specs)
-        # all_labels.extend([0] * len(specs))
 
-    # class1
     for clip in class1:
         specs = process_data(clip, clip_length, 1)
-        # all_spectograms.extend(specs)
-        # all_labels.extend([1] * len(specs))
 
-    # X = np.array(all_spectograms)
-    # Y = np.array(all_labels)
-    # print(f"X shape: {X.shape}")
-    # print(f"Y shape: {Y.shape}")
-    #
-    # #save to a file
-    # np.savez_compressed("./audio_data.npz", X=X, Y=Y)
-    # print("data saved")
 
 create_directories()
 convert_to_spectograms(3)
