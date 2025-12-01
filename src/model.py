08f8ebdeeb91651386694695c0b274e3289743b5
BanhBaoz <q.vuvan05@gmail.com>
2025-12-02 00:00:01 +0100

    model

--- src/model.py
+++ src/model.py
@@ -8,16 +8,33 @@ import torch.nn.functional as F
 class SimpleCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)   # input: 1 channel spectrogram (grayscale)
+        self.conv1 = nn.Sequential( nn.Conv2d(1, 16, 5), #
+                                                       nn.BatchNorm2d(16), nn.ReLU(),
+                                                       nn.BatchNorm2d(16), nn.MaxPool2d(2, 2) )
+        self.conv2 = nn.Sequential( nn.Conv2d(16, 32, 5), #
+                                    nn.BatchNorm2d(32), nn.ReLU(), nn.BatchNorm2d(32),
+                                    nn.MaxPool2d(2, 2) )
+        self.conv3 = nn.Sequential( nn.Conv2d(32, 64, 5), #
+                                    nn.BatchNorm2d(64), nn.ReLU(), nn.BatchNorm2d(64),
+                                    nn.MaxPool2d(2, 2) )
+        # self.conv4 = nn.Sequential( # nn.Conv2d(64, 128, 5),
+                                      # #nn.BatchNorm2d(128),
+                                      # nn.ReLU(),
+                                      # nn.BatchNorm2d(128),
+                                      # nn.MaxPool2d(2, 2) # )
+        # input: 1 channel spectrogram (grayscale)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(13456, 120)
+        self.fc1 = nn.Linear(9216, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)  # binary output
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
+        self.fc3 = nn.Linear(84, 1)
+        # binary output
+    def forward(self, x): #
+        x = self.pool(F.relu(self.conv1(x))) #
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        # x = self.conv4(x)
         x = x.view(x.shape[0], -1) #a bit more robust
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
