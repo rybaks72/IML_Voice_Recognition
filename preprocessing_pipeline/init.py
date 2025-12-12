2bdad3edb9d166db9ed900e74c1d9b4f7f5f44ed
BanhBaoz <q.vuvan05@gmail.com>
2025-12-13 00:56:57 +0100

    update

--- preprocessing_pipeline/init.py
+++ preprocessing_pipeline/init.py
@@ -15,8 +15,8 @@ def get_local_version():
 
 def get_kaggle_dataset_version():
     import kaggle
-    owner, dataset = DATASET.split("/")
-    meta = kaggle.api.dataset_metadata(owner, dataset)
+    owner, dt = DATASET.split("/")
+    meta = kaggle.api.dataset_metadata(owner, dt)
     return meta['versionNumber']
 
 def download():
